3a50cecf818b2d29632428ae257732ff1c0ec9e8
Mateusfagundes123 <mateustfagundes007@gmail.com>
2026-05-17 20:19:36 -0300

    ajuste nos caminhos

--- JS/loadComponents.js
+++ JS/loadComponents.js
@@ -1,8 +1,20 @@
-fetch("../components/header.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
-  });
+let caminhoHeader = "";
+
+if (window.location.pathname.includes("/HTML/")) {
+    caminhoHeader = "../components/header.html";
+} else {
+    caminhoHeader = "./components/header.html";
+}
+
+fetch(caminhoHeader)
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
+
+function abrirMenu() {
+    document.getElementById("menu").classList.toggle("ativo");
+}
   
   function abrirMenu() {
     document.getElementById("menu").classList.toggle("ativo");
